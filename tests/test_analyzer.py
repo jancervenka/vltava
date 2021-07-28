4c946635d4cddcff83b56217de5a2ec9dec35b86
Jan Červenka <jan.cervenka@yahoo.com>
2021-07-28 22:30:03 +0200

    inital commit

---
+++ tests/test_analyzer.py
@@ -0,0 +1,15 @@
+import pytest
+from src import _MajkaAnalyzer, _MorphoditaAnalyzer
+
+
+@pytest.mark.parametrize(
+    "analyzer_class, raw_word, expected",
+    [
+        (_MajkaAnalyzer, "nejlepsi", "dobrý"),
+        (_MajkaAnalyzer, "xxxzzz", "xxxzzz"),
+        (_MorphoditaAnalyzer, "nejlepsi", "dobrý"),
+        (_MorphoditaAnalyzer, "zzz123", "zzz123"),
+    ]
+)
+def test_get_lemma(analyzer_class, raw_word, expected):
+    assert analyzer_class().get_lemma(raw_word) == expected
---
+++ tests/test_document_processor.py
@@ -0,0 +1,23 @@
+import pytest
+from vltava import DocumentProcessor
+
+
+@pytest.mark.parametrize(
+    "doc, tokenize, expected",
+    [
+        ("ahoj, to je Test. nejlepší", True, ["test", "dobry"]),
+        ("<div>ahoj, to máš 22342442 xaaa</div> Piva", False, "xaaa pivo")
+    ]
+)
+def test_process(doc, tokenize, expected):
+
+    assert DocumentProcessor().process(doc, tokenize) == expected
+
+
+def test_process_from_iterable():
+
+    docs = ["dnes test dokumenty ... piva 1", "89 počítače"] * 10
+    expected = [['test', 'dokument', 'pivo'], ["pocitace"]] * 10
+
+    result = DocumentProcessor().process_from_iterable(docs, n_jobs=2)
+    assert result == expected
